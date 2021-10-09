ed9783a12f4889ddbc550a72c5449eaa0025f347
codeSilverWolf <96832649+codeSilverWolf@users.noreply.github.com>
2021-10-09 16:41:08 +0200

    Buildsystem changes for msys2:  Make Cmake detect libraries from msys.  Disable building tests and hkxcmd.  Enable /bigobj equivalent for gcc linker. Update .gitignore for vscodium. Disable lto to save some link time. Add doxygen target for documentation.

--- CMake/Boost.cmake
+++ CMake/Boost.cmake
@@ -1,16 +1,18 @@
-include (FetchContent)
+# Disable fetching boost library, we will use package from msys
 
-if(${UseLocalBoost})
-    message("Boost: Using local copy of Boost at ${BOOST_ROOT}")
-    set(FETCHCONTENT_SOURCE_DIR_BOOST ${BOOST_ROOT})
-endif()
+# include (FetchContent)
 
-FetchContent_Declare(
-  BoostCMake
-  GIT_REPOSITORY https://github.com/Guekka/boost-cmake.git
-)
+# if(${UseLocalBoost})
+#     message("Boost: Using local copy of Boost at ${BOOST_ROOT}")
+#     set(FETCHCONTENT_SOURCE_DIR_BOOST ${BOOST_ROOT})
+# endif()
 
-FetchContent_MakeAvailable(BoostCMake)
+# FetchContent_Declare(
+#   BoostCMake
+#   GIT_REPOSITORY https://github.com/Guekka/boost-cmake.git
+# )
+
+# FetchContent_MakeAvailable(BoostCMake)
 
 add_library(BoostNemesis INTERFACE)
 target_link_libraries(BoostNemesis INTERFACE Boost::regex)
--- CMake/Python.cmake
+++ CMake/Python.cmake
@@ -2,58 +2,70 @@
 # VARIABLES - Pre download
 ################################################################################
 
-set(Python_VERSION 3.8.2)
-set(Python_LIB_VERSION 38)
+set(Python_VERSION 3.9.7)
+set(Python_LIB_VERSION 39)
 
 
 ################################################################################
 # Download Python
 ################################################################################
 
-include(FetchContent)
+# Disable fetching python source
+# include(FetchContent)
 
-if(NOT UseLocalPython)
-    message(STATUS Downloading CPython)
+# if(NOT UseLocalPython)
+#     message(STATUS Downloading CPython)
 
-    FetchContent_Declare(
-        CPython
-        GIT_REPOSITORY https://github.com/python/cpython.git
-        GIT_TAG v${Python_VERSION}
-        )
+#     FetchContent_Declare(
+#         CPython
+#         GIT_REPOSITORY https://github.com/python/cpython.git
+#         GIT_TAG v${Python_VERSION}
+#         )
 
-    if(NOT CPython_POPULATED)
-      FetchContent_Populate(CPython)
-    endif()
+#     if(NOT CPython_POPULATED)
+#       FetchContent_Populate(CPython)
+#     endif()
 
-    set(PYTHON_ROOT ${cpython_SOURCE_DIR} CACHE PATH "Python local path" FORCE)
+#     set(PYTHON_ROOT ${cpython_SOURCE_DIR} CACHE PATH "Python local path" FORCE)
+# endif()
+
+find_package (Python COMPONENTS Development )
+
+if (Python_FOUND)
+    message(STATUS "Python found: ${Python_VERSION}")
+    message(STATUS "Python include dirs : ${Python_INCLUDE_DIRS}")
+
 endif()
+
 
 ################################################################################
 # VARIABLES - Post download
 ################################################################################
-set(Python_INCLUDE_DIRS "${PYTHON_ROOT}/PC" "${PYTHON_ROOT}/Include")
+# set(Python_INCLUDE_DIRS "${PYTHON_ROOT}/PC" "${PYTHON_ROOT}/Include")
 
-set(Python_BUILD_DIR_ROOT ${PYTHON_ROOT}/PCBuild)
-set(Python_BUILD_FILE ${Python_BUILD_DIR_ROOT}/build.bat)
+# set(Python_BUILD_DIR_ROOT ${PYTHON_ROOT}/PCBuild)
+# set(Python_BUILD_FILE ${Python_BUILD_DIR_ROOT}/build.bat)
 
-set(Python_BUILD_DIR ${Python_BUILD_DIR_ROOT}/win32)
-set(Python_Args "-p" "x86")
+# set(Python_BUILD_DIR ${Python_BUILD_DIR_ROOT}/win32)
+# set(Python_Args "-p" "x86")
 
 #Configure based on build type
 
-if(${CMAKE_BUILD_TYPE} MATCHES Debug)
-    set(Python_LIBRARIES ${Python_BUILD_DIR}/python${Python_LIB_VERSION}_d.lib)
-    set(Python_Args ${Python_Args} "-c" "Debug")
-    set(Python_DLL_NAME Python${Python_LIB_VERSION}_d.dll)
-else()
-    set(Python_LIBRARIES ${Python_BUILD_DIR}/python${Python_LIB_VERSION}.lib)
-    set(Python_Args ${Python_Args} "-c" "Release")
-    set(Python_DLL_NAME Python${Python_LIB_VERSION}.dll)
-endif()
+# if(${CMAKE_BUILD_TYPE} MATCHES Debug)
+#     set(Python_LIBRARIES ${Python_BUILD_DIR}/python${Python_LIB_VERSION}_d.lib)
+#     set(Python_Args ${Python_Args} "-c" "Debug")
+#     set(Python_DLL_NAME Python${Python_LIB_VERSION}_d.dll)
+# else()
+#     set(Python_LIBRARIES ${Python_BUILD_DIR}/python${Python_LIB_VERSION}.lib)
+#     set(Python_Args ${Python_Args} "-c" "Release")
+#     set(Python_DLL_NAME Python${Python_LIB_VERSION}.dll)
+# endif()
 
+
+# Disable building python in msys
 #Build
-message("Building CPython:    File: ${Python_BUILD_FILE}   Args: ${Python_Args}")
-execute_process(COMMAND ${Python_BUILD_FILE} ${Python_Args})
+# message("Building CPython:    File: ${Python_BUILD_FILE}   Args: ${Python_Args}")
+# execute_process(COMMAND ${Python_BUILD_FILE} ${Python_Args})
 
 
 ################################################################################
@@ -69,4 +81,9 @@ target_link_libraries(PythonNemesis INTERFACE ${Python_LIBRARIES})
 # Copy DLL
 ################################################################################
 
-set(Python_DLL_PATH ${Python_BUILD_DIR}/${Python_DLL_NAME})
+#set(Python_DLL_PATH ${Python_BUILD_DIR}/${Python_DLL_NAME})
+
+# path to python dll in msys
+set(Python_DLL_NAME libpython${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}.dll)
+set(Python_DLL_PATH "${Python_ROOT_DIR}/bin/${Python_DLL_NAME}")
+message(STATUS "Python dll path: ${Python_DLL_PATH}")
