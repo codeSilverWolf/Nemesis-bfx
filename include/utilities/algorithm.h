49cde232f24d7ff12fe81915b15178e8a66a3b84
codeSilverWolf <96832649+codeSilverWolf@users.noreply.github.com>
2021-12-10 03:41:38 +0100

    [BUGFIX] Fix memory leaks in algorithm.cpp
    Cppcheck warns that nothing is releasing memory allocated in
    nemesis::to_lower_copy and it's right.
    Comment out leak prone versions of functions that use char* for strings,
    and make std::string/wstring versions primary.
    Change algorithms to avoid unneded copying.
    
    TODO: change nemesis::transform_too template to be UTF aware.
    Generally all functions in this module must be UTF-8/UTF-16 aware and
    optimized!

--- include/utilities/algorithm.h
+++ include/utilities/algorithm.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <iterator>
 #include <string>
 
 namespace nemesis
 {
-    const char* to_lower_copy(const char* data);
-    const wchar_t* to_lower_copy(const wchar_t* data);
+    //TODO: remove unneded declarations
+
+    //const char* to_lower_copy(const char* data);
+    //const wchar_t* to_lower_copy(const wchar_t* data);
 
     std::string to_lower_copy(const std::string& data);
     std::wstring to_lower_copy(const std::wstring& data);
@@ -13,8 +16,8 @@ namespace nemesis
     void to_lower(std::string& data);
     void to_lower(std::wstring& data);
 
-    const char* to_upper_copy(const char* data);
-    const wchar_t* to_upper_copy(const wchar_t* data);
+    //const char* to_upper_copy(const char* data);
+    //const wchar_t* to_upper_copy(const wchar_t* data);
 
     std::string to_upper_copy(const std::string& data);
     std::wstring to_upper_copy(const std::wstring& data);
@@ -22,21 +25,36 @@ namespace nemesis
     void to_upper(std::string& data);
     void to_upper(std::wstring& data);
 
-    bool iequals(const char* l, const char* r);
-    bool iequals(const wchar_t* l, const wchar_t* r);
-    bool iequals(const char* l, const std::string& r);
-    bool iequals(const wchar_t* l, const std::wstring& r);
-    bool iequals(const std::string& l, const char* r);
-    bool iequals(const std::wstring& l, const wchar_t* r);
+    // bool iequals(const char* l, const char* r);
+    // bool iequals(const wchar_t* l, const wchar_t* r);
     bool iequals(const std::string& l, const std::string& r);
     bool iequals(const std::wstring& l, const std::wstring& r);
+    // bool iequals(const char* l, const std::string& r);
+    // bool iequals(const wchar_t* l, const std::wstring& r);
+    // bool iequals(const std::string& l, const char* r);
+    // bool iequals(const std::wstring& l, const wchar_t* r);
+
+    // template <typename T, typename F>
+    // inline T transform_to(const F& str) noexcept
+    // {
+    //     if (str.empty()) return {};
+
+    //     return {std::begin(str), std::end(str)};
+    // };
+
+    // TODO: transform to doesnt care about conversions from UTF-16 to std::string aka UTF-8 !!! is this ok?
+    // at least we should check for characters out of ascii range and report warning. 
 
     template <typename T, typename F>
     inline T transform_to(const F& str) noexcept
     {
         if (str.empty()) return {};
-
-        return {std::begin(str), std::end(str)};
+        T str_out;
+        auto iter_end = std::end(str);
+        for(auto i = std::begin(str); i != iter_end; ++i)
+            str_out.push_back(*i);
+        return str_out;
     };
+
 
 } // namespace nemesis
--- src/utilities/algorithm.cpp
+++ src/utilities/algorithm.cpp
@@ -11,143 +11,215 @@ using namespace std;
 
 namespace nemesis
 {
-    const char* to_lower_copy(const char* data)
-    {
-        char* temp = _strdup(data);
-        int size   = strlen(data);
-        int i      = 0;
+    //TODO: optimize iequals and to_*_copy functions
+    // remove no longer needed overloads
 
-        while (i < size)
-        {
-            temp[i] = tolower(temp[i]);
-            i++;
-        }
+    // const char* to_lower_copy(const char* data)
+    // {
+    //     char* temp = _strdup(data);
+    //     int size   = strlen(data);
+    //     int i      = 0;
 
-        return temp;
-    }
+    //     while (i < size)
+    //     {
+    //         temp[i] = tolower(temp[i]);
+    //         i++;
+    //     }
 
-    const wchar_t* to_lower_copy(const wchar_t* data)
-    {
-        wchar_t* temp = _wcsdup(data);
-        int size      = wcslen(data);
-        int i         = 0;
+    //     return temp;
+    // }
 
-        while (i < size)
-        {
-            temp[i] = tolower(temp[i]);
-            i++;
-        }
+    // const wchar_t* to_lower_copy(const wchar_t* data)
+    // {
+    //     wchar_t* temp = _wcsdup(data);
+    //     int size      = wcslen(data);
+    //     int i         = 0;
 
-        return temp;
-    }
+    //     while (i < size)
+    //     {
+    //         temp[i] = tolower(temp[i]);
+    //         i++;
+    //     }
+
+    //     return temp;
+    // }
 
     string to_lower_copy(const string& data)
     {
-        return to_lower_copy(data.c_str());
+        string::size_type str_len = data.length();
+
+        if(str_len == 0) return {};
+        
+        string tmpstr = data;
+        for( string::size_type i = 0; i < str_len; i++)
+        {
+            tmpstr[i] = tolower(data[i]);
+        }
+        return tmpstr;
     }
 
     wstring to_lower_copy(const wstring& data)
     {
-        return to_lower_copy(data.c_str());
+        wstring::size_type str_len = data.length();
+
+        if(str_len == 0) return {};
+        
+        wstring tmpstr = data;
+        for( wstring::size_type i = 0; i < str_len; i++)
+        {
+            tmpstr[i] = towlower(data[i]);
+        }
+        return tmpstr;
     }
 
     void to_lower(string& data)
     {
-        data = to_lower_copy(data.c_str());
+        data = to_lower_copy(data);
     }
 
     void to_lower(wstring& data)
     {
-        data = to_lower_copy(data.c_str());
+        data = to_lower_copy(data);
     }
 
-    const char* to_upper_copy(const char* data)
-    {
-        char* temp = _strdup(data);
-        int size   = strlen(data);
-        int i      = 0;
+    // const char* to_upper_copy(const char* data)
+    // {
+    //     char* temp = _strdup(data);
+    //     int size   = strlen(data);
+    //     int i      = 0;
 
-        while (i < size)
-        {
-            temp[i] = toupper(temp[i]);
-            i++;
-        }
+    //     while (i < size)
+    //     {
+    //         temp[i] = toupper(temp[i]);
+    //         i++;
+    //     }
 
-        return temp;
-    }
+    //     return temp;
+    // }
 
-    const wchar_t* to_upper_copy(const wchar_t* data)
-    {
-        wchar_t* temp = _wcsdup(data);
-        int size      = wcslen(data);
-        int i         = 0;
+    // const wchar_t* to_upper_copy(const wchar_t* data)
+    // {
+    //     wchar_t* temp = _wcsdup(data);
+    //     int size      = wcslen(data);
+    //     int i         = 0;
 
-        while (i < size)
-        {
-            temp[i] = toupper(temp[i]);
-            i++;
-        }
+    //     while (i < size)
+    //     {
+    //         temp[i] = toupper(temp[i]);
+    //         i++;
+    //     }
 
-        return temp;
-    }
+    //     return temp;
+    // }
 
     string to_upper_copy(const string& data)
     {
-        return to_upper_copy(data.c_str());
+        string::size_type str_len = data.length();
+
+        if(str_len == 0) return {};
+        
+        string tmpstr = data;
+        for( string::size_type i = 0; i < str_len; i++)
+        {
+            tmpstr[i] = toupper(data[i]);
+        }
+        return tmpstr;
     }
 
     wstring to_upper_copy(const wstring& data)
     {
-        return to_upper_copy(data.c_str());
+        wstring::size_type str_len = data.length();
+
+        if(str_len == 0) return {};
+        
+        wstring tmpstr = data;
+        for( wstring::size_type i = 0; i < str_len; i++)
+        {
+            tmpstr[i] = towlower(data[i]);
+        }
+        return tmpstr;
     }
 
     void to_upper(string& data)
     {
-        data = to_upper_copy(data.c_str());
+        data = to_upper_copy(data);
     }
 
     void to_upper(wstring& data)
     {
-        data = to_upper_copy(data.c_str());
+        data = to_upper_copy(data);
     }
 
-    bool iequals(const char* l, const char* r)
-    {
-        return strcmp(to_lower_copy(l), to_lower_copy(r)) == 0;
-    }
+    // bool iequals(const char* l, const char* r)
+    // {
+    //     return strcmp(to_lower_copy(l).c_str(), to_lower_copy(r).c_str()) == 0;
+    // }
 
-    bool iequals(const wchar_t* l, const wchar_t* r)
-    {
-        return wcscmp(to_lower_copy(l), to_lower_copy(r)) == 0;
-    }
-
-    bool iequals(const char* l, const string& r)
-    {
-        return strcmp(to_lower_copy(l), to_lower_copy(r.c_str())) == 0;
-    }
-
-    bool iequals(const wchar_t* l, const wstring& r)
-    {
-        return wcscmp(to_lower_copy(l), to_lower_copy(r.c_str())) == 0;
-    }
-
-    bool iequals(const string& l, const char* r)
-    {
-        return strcmp(to_lower_copy(l.c_str()), to_lower_copy(r)) == 0;
-    }
-
-    bool iequals(const wstring& l, const wchar_t* r)
-    {
-        return wcscmp(to_lower_copy(l.c_str()), to_lower_copy(r)) == 0;
-    }
+    // bool iequals(const wchar_t* l, const wchar_t* r)
+    // {
+    //     return wcscmp(to_lower_copy(l).c_str(), to_lower_copy(r)) == 0;
+    // }
 
     bool iequals(const string& l, const string& r)
     {
-        return strcmp(to_lower_copy(l.c_str()), to_lower_copy(r.c_str())) == 0;
+        string::size_type l_length = l.length(), r_length = r.length();
+
+        if(l_length == r_length)
+        {
+            string::size_type i = 0;
+            while(i < l_length)
+            {
+                if (tolower(l[i]) != tolower(r[i]))
+                    return false;
+                i++;
+            }
+            return true; // both strings equal
+        }
+        else
+        {
+            return false;   // strings of different length can not be equal
+        }
     }
 
     bool iequals(const wstring& l, const wstring& r)
     {
-        return wcscmp(to_lower_copy(l.c_str()), to_lower_copy(r.c_str())) == 0;
+        wstring::size_type l_length = l.length(), r_length = r.length();
+
+        if(l_length == r_length)
+        {
+            wstring::size_type i = 0;
+            while(i < l_length)
+            {
+                if (tolower(l[i]) != tolower(r[i]))
+                    return false;
+                i++;
+            }
+            return true; // both strings equal
+        }
+        else
+        {
+            return false;   // strings of different length can not be equal
+        }
     }
+
+    // bool iequals(const char* l, const string& r)
+    // {
+    //     return iequals(string(l), r);
+    // }
+
+    // bool iequals(const wchar_t* l, const wstring& r)
+    // {
+    //     return iequals(wstring(l), r);
+    // }
+
+    // bool iequals(const string& l, const char* r)
+    // {
+    //     return iequals(l, string(r));
+    // }
+
+    // bool iequals(const wstring& l, const wchar_t* r)
+    // {
+    //     return iequals(l, wstring(r));
+    // }
+
 } // namespace nemesis
