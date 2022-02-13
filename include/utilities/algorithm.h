62f6cc71db3e5ee7ddde1e748dda6de1aa5d3c6c
codeSilverWolf <96832649+codeSilverWolf@users.noreply.github.com>
2022-02-13 18:30:28 +0100

    [UNICODE] Add UTF8<=>UTF16 nemesis::transform_to templates
    This commit adds specializations that can correctly transform
    between multibyte UTF-8 std::string and UTF-16 std::wstring.

--- include/utilities/algorithm.h
+++ include/utilities/algorithm.h
@@ -2,6 +2,8 @@
 
 #include <iterator>
 #include <string>
+#include <cwchar>
+#include <vector>
 
 namespace nemesis
 {
@@ -42,8 +44,8 @@ namespace nemesis
     //     return {std::begin(str), std::end(str)};
     // };
 
-    // TODO: transform to doesnt care about conversions from UTF-16 to std::string aka UTF-8 !!! is this ok?
-    // at least we should check for characters out of ascii range and report warning. 
+    // TODO: generic transform_to will mangle non ascii ancoded strings!
+    // Add check for this situation and throw exception. 
 
     template <typename T, typename F>
     inline T transform_to(const F& str) noexcept
@@ -55,6 +57,39 @@ namespace nemesis
             str_out.push_back(*i);
         return str_out;
     };
+
+    // UTF-8 -> UTF-16 version
+    // TODO: see if we can optimize memory allocation by using, for small strings,
+    // static buffer instead of std::vector. Maybe array on stack?
+
+    template <>
+        inline std::wstring transform_to(const std::string& str) noexcept
+        {
+            if (str.empty()) return {};
+            std::mbstate_t shift_state{};
+            const char *input_buf = str.c_str();
+            std::size_t output_len = mbsrtowcs(nullptr, &input_buf, 0, &shift_state) + 1;
+
+            std::vector<wchar_t> out_buffer(output_len);
+            mbsrtowcs(&out_buffer[0], &input_buf, output_len, &shift_state);
+            return std::wstring(out_buffer.data());
+        };
+
+    // UTF-16 -> UTF-8 version
+
+    template <>
+        inline std::string transform_to(const std::wstring& str) noexcept
+        {
+            if (str.empty()) return {};
+            std::mbstate_t shift_state{};
+            const wchar_t *input_buf = str.c_str();
+            std::size_t output_len = wcsrtombs(nullptr, &input_buf, 0, &shift_state) + 1;
+
+            std::vector<char> out_buffer(output_len);
+            wcsrtombs(&out_buffer[0], &input_buf, output_len, &shift_state);
+            
+            return std::string(out_buffer.data());
+        };
 
 
 } // namespace nemesis
